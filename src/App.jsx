1bcd39ec5493b623147f89e0f61ce31cf5ef73a8
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-10-28 16:52:22 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FinanceApp from './components/FinanceApp';
 
-const API_URL = 'https://your-backend.onrender.com/api'; // Вставьте URL вашего backend
+const API_URL = 'https://walletback-aghp.onrender.com'; // Вставьте URL вашего backend
 
 export default function App() {
   return <FinanceApp apiUrl={API_URL} />;
