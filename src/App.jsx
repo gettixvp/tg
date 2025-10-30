22247df75491432ede13833fe644ee8a680adcf5
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-10-30 08:58:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+// src/App.jsx
+import FinanceApp from './components/FinanceApp';
+
 const API_URL = 'https://walletback-aghp.onrender.com'; // БЕЗ /api
 
 export default function App() {
-  return (
-    <div id="app">
-      <FinanceApp apiUrl={API_URL} />
-    </div>
-  );
+  return <FinanceApp apiUrl={API_URL} />;
 }
