086e80d166c28fae7b6f64a336afbfb0c3598f0c
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-10-30 09:04:31 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  darkMode: 'class',
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
