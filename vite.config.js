13d3fa25bd7d468dfdaa093f7228d3f2ee6ffeb6
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-10-30 09:03:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  server: {
+    port: 5173,
+    host: '0.0.0.0'
+  },
   build: {
     outDir: 'dist',
-  },
-  // Указываем, где искать index.html
-  root: '.', // корень проекта
+    sourcemap: true
+  }
 });
