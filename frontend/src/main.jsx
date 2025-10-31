93e46cc4da8b5d3b5a4d2840f6beeaa326f0046e
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-10-31 11:56:06 +0300

    1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import '../index.css'; // <-- Исправленный путь
 
 // === Chart.js ===
 import Chart from 'chart.js/auto';
